b922e45ee4e60a8017c869a00bcf074dfa725bfa
Martin Quintana <marquiran@freimartnsarmiento.com>
2022-09-16 13:08:01 +0200

    Update prueba4.py

--- Unidad 0 Introduccion a Python3/Ejercicios/Realizacion de ejercicios/Enunciados 2/prueba4.py
+++ Unidad 0 Introduccion a Python3/Ejercicios/Realizacion de ejercicios/Enunciados 2/prueba4.py
@@ -1,7 +1,36 @@
 import random
 
-numRandom = int(random.randint(1, 10))
+LIMITE = 7.5
+baraja = [1, 2, 3, 4, 5, 6, 7, 0.5, 0.5, 0.5] * 4
+mano = []
+play = True
 
-jugador1 = input('Introduce nombre jugador 1: ')
-jugador2 = input('Introduce nombre jugador 2: ')
+
+while play:
+    # Pide y guarda el numero
+    posicion_aleatoria = random.randint(0, len(baraja) - 1)
+    carta_aletatorio = baraja.pop(posicion_aleatoria)
+    mano.append(carta_aletatorio)
+    print('Mi mano: {mano}'.format(mano=mano))
+
+    # Sumamos mi mano
+    suma_total = 0
+    for carta in mano:
+        suma_total += carta
+
+    print('Tengo {total} puntos'.format(total=suma_total))
+
+    # Preguntar si nos queremos plantar
+    respuesta = input('¿Te plantas?: ')
+    respuesta.lower()
+    if respuesta == 'si':
+        play = False
+    else:
+        # Comprobamos si ha ganado o perdido
+        if suma_total > LIMITE:
+            print('Has perdido con {total}'.format(total=suma_total))
+            play = False
+        elif suma_total == LIMITE:
+            print('Has ganado!!!')
+            play = False
 
