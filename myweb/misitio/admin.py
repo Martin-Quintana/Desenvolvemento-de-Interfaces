7783e8f007777dfd3f9be70c7a3b56ff2ecf251b
Martin Quintana <martinquintanarana@gmail.com>
2022-11-04 13:01:21 +0100

    04/11/22

--- myweb/misitio/admin.py
+++ myweb/misitio/admin.py
@@ -1,4 +1,5 @@
+from .models import Cliente
 from django.contrib import admin
 
-from .models import Cliente
 admin.site.register(Cliente)
+
--- myweb/misitio/urls.py
+++ myweb/misitio/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.clientes_list, name='clientes_list')
+    path('', views.clientes_list, name='clientes_list'),
 ]
+
