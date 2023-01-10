44a2956b1e9f85a12ac57b37b7e74b2548188c8d
Martin Quintana <martinquintanarana@gmail.com>
2023-01-10 13:56:57 +0100

    10/01/23

--- QuintanaRanha/informes.py
+++ QuintanaRanha/informes.py
@@ -12,33 +12,34 @@ class Informes:
         try:
             var.report = canvas.Canvas('informes/listadoClientes.pdf')
             titulo = 'LISTADO CLIENTES'
-            var.report.drawString(100, 750, titulo)
-
-            var.report.save()
+            var.report.drawString(250, 685, titulo)
             Informes.pieInforme(titulo)
+            Informes.cabecera(titulo)
+            var.report.save()
             rootPath = '.\\informes'
 
             for file in os.listdir(rootPath):
                 if file.endswith('Clientes.pdf'):
                     os.startfile('%s\%s' % (rootPath, file))
         except Exception as error:
-            print('Error informes estado de clientes' , error)
+            print('Error informes estado de clientes', error)
 
     def listAutos(self):
 
         try:
             var.report = canvas.Canvas('informes/listadoAutos.pdf')
             titulo = 'LISTADO VEHICULO'
-            var.report.drawString(100, 750, titulo)
-            var.report.save()
+            var.report.drawString(250, 685, titulo)
             Informes.pieInforme(titulo)
+            Informes.cabecera(titulo)
+            var.report.save()
             rootPath = '.\\informes'
 
             for file in os.listdir(rootPath):
                 if file.endswith('Autos.pdf'):
                     os.startfile('%s\%s' % (rootPath, file))
         except Exception as error:
-            print('Error informes estado de vehiculos' , error)
+            print('Error informes estado de vehiculos', error)
 
     def pieInforme(titulo):
 
@@ -51,4 +52,33 @@ class Informes:
             var.report.drawString(250, 40, str(titulo))
             var.report.drawString(460, 40, str('Pagina %s' % var.report.getPageNumber()))
         except Exception as error:
-            print('Error pie informe de cualquier tipo ' , error)
+            print('Error pie informe de cualquier tipo ', error)
+
+    def cabecera(titulo):
+        try:
+            logo = '.\img\logo.png'
+            var.report.line(50, 800, 525, 800)
+            var.report.setFont('Helvetica', size=10)
+
+            var.report.setAuthor('Martin Quintana')
+
+            textCIF = 'A0000000H'
+            textnom = 'TALLER MECANICO TEIS'
+            textdir = 'Avenida de Galicia, 101'
+            textpost = 'Vigo - 36216 - España'
+            textemail = 'iesteis@iesteis.es'
+            texttlfo = 'Telefono: 886 12 04 64'
+            var.report.drawString(50, 805, textCIF)
+            var.report.drawString(50, 785, textnom)
+            var.report.drawString(50, 765, textdir)
+            var.report.drawString(50, 750, textpost)
+            var.report.drawString(50, 735, texttlfo)
+            var.report.drawString(50, 720, textemail)
+            var.report.drawImage(logo, 400, 725, width=150, height= 70)
+            var.report.line(50, 710, 525, 710)
+
+
+
+
+        except Exception as error:
+            print('Error en la cabecera clase Informes ', error)
