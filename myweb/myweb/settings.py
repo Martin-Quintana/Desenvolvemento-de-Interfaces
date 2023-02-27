8a768f81c505123fb44d53ddcb9c20265619e5fd
martinuxx18 <martinquintanarana@gmail.com>
2023-02-27 18:41:08 +0100

    las commit

--- myweb/myweb/settings.py
+++ myweb/myweb/settings.py
@@ -22,6 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-5&6d$pk4ydl#xtgtm=pivx8=a6-a@fo%nd8f_fg7*n=(h=ampw'
 
+
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
